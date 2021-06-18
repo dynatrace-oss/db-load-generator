8f59deff1301b5cce261b25b38a94bff569574a6
Vagiz Duseev <vagiz.duseev@dynatrace.com>
2021-06-18 14:40:19 +0200

    Bump version: 0.8.5 → 0.8.6

--- docs/conf.py
+++ docs/conf.py
@@ -23,7 +23,7 @@ copyright = f"2020–{datetime.now().year}, Dynatrace LLC"
 author = "Vagiz Duseev <vagiz.duseev@dynatrace.com>"
 
 # Short version
-version = "0.8.5"
+version = "0.8.6"
 # The full version, including alpha/beta/rc tags
 release = version
 
